6449301b0e8437b60f50de415f6ac629cc014326
Vitaly Medvedenko <vitalik.med2018@gmail.com>
2026-01-30 19:11:46 +0700

    Initial commit

---
+++ src/main/java/ru/sibsutis/vet_gateway/VetGatewayApplication.java
@@ -0,0 +1,13 @@
+package ru.sibsutis.vet_gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VetGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VetGatewayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/sibsutis/vet_gateway/VetGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.sibsutis.vet_gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VetGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
