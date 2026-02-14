4c635ac0cdf368c75e588048ab63440ccb9ca390
Vitaly Medvedenko <vitalik.med2018@gmail.com>
2026-02-15 00:18:34 +0700

    Bring to working version

---
+++ src/main/java/ru/sibsutis/vet_gateway/configuration/SecurityConfig.java
@@ -0,0 +1,26 @@
+package ru.sibsutis.vet_gateway.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+
+@Configuration
+@EnableMethodSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityWebFilterChain apiFilterChain(ServerHttpSecurity http) {
+        http
+                .authorizeExchange(exchange -> exchange
+                        .anyExchange().authenticated()
+                )
+                .oauth2ResourceServer(oauth2 -> oauth2
+                        .jwt(Customizer.withDefaults())
+                );
+
+        return http.build();
+    }
+}
