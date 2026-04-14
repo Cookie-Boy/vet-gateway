91b75d41af4a2749ccf26e2b8e2241e6b409acb9
Vitaly Medvedenko <vitalik.med2018@gmail.com>
2026-04-15 04:05:49 +0700

    Fix routes

--- src/main/java/ru/sibsutis/vet_gateway/configuration/SecurityConfig.java
+++ src/main/java/ru/sibsutis/vet_gateway/configuration/SecurityConfig.java
@@ -5,20 +5,27 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.oauth2.jwt.ReactiveJwtDecoder;
 import org.springframework.security.web.server.SecurityWebFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.CorsConfigurationSource;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
 
 @Configuration
 @EnableMethodSecurity
 public class SecurityConfig {
 
     @Bean
-    public SecurityWebFilterChain apiFilterChain(ServerHttpSecurity http) {
+    public SecurityWebFilterChain apiFilterChain(ServerHttpSecurity http, ReactiveJwtDecoder jwtDecoder) {
         http
+                .csrf(ServerHttpSecurity.CsrfSpec::disable)
                 .authorizeExchange(exchange -> exchange
                         .anyExchange().authenticated()
                 )
                 .oauth2ResourceServer(oauth2 -> oauth2
-                        .jwt(Customizer.withDefaults())
+                        .jwt(jwt -> jwt.jwtDecoder(jwtDecoder))
                 );
 
         return http.build();
