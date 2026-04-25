1b54b125b421e626966955049c8921f3ca4521b5
Vitaly Medvedenko <vitalik.med2018@gmail.com>
2026-04-25 14:51:14 +0700

    Add CORS

---
+++ src/main/java/ru/sibsutis/vet_gateway/configuration/CorsConfig.java
@@ -0,0 +1,28 @@
+package ru.sibsutis.vet_gateway.configuration;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.CorsWebFilter;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+
+@Configuration
+public class CorsConfig {
+
+    @Value("${frontend.base.url}")
+    private String frontendBaseUrl;
+
+    @Bean
+    public CorsWebFilter corsWebFilter() {
+        CorsConfiguration corsConfig = new CorsConfiguration();
+        corsConfig.addAllowedOrigin(frontendBaseUrl);
+        corsConfig.addAllowedMethod("*");
+        corsConfig.addAllowedHeader("*");
+        corsConfig.setAllowCredentials(true);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", corsConfig);
+        return new CorsWebFilter(source);
+    }
+}
