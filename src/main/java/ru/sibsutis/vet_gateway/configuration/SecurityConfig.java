d27227d78ca23ae50ea0693fbc5198da1b5476f6
Vitaly Medvedenko <vitalik.med2018@gmail.com>
2026-06-25 17:59:27 +0700

    Add new frontend domain

--- src/main/java/ru/sibsutis/vet_gateway/configuration/SecurityConfig.java
+++ src/main/java/ru/sibsutis/vet_gateway/configuration/SecurityConfig.java
@@ -3,16 +3,10 @@ package ru.sibsutis.vet_gateway.configuration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.oauth2.jwt.ReactiveJwtDecoder;
 import org.springframework.security.web.server.SecurityWebFilterChain;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.reactive.CorsConfigurationSource;
-import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
-
-import java.util.Arrays;
 
 @Configuration
 @EnableMethodSecurity
