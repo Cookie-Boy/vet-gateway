dd6fba945a955690045d67f3838f83cf050f53f8
Vitaly Medvedenko <vitalik.med2018@gmail.com>
2026-06-28 16:26:09 +0700

    Add qr-scan public endpoint

--- src/main/java/ru/sibsutis/vet_gateway/configuration/SecurityConfig.java
+++ src/main/java/ru/sibsutis/vet_gateway/configuration/SecurityConfig.java
@@ -18,12 +18,12 @@ public class SecurityConfig {
                 .csrf(ServerHttpSecurity.CsrfSpec::disable)
                 .authorizeExchange(exchange -> exchange
                         .pathMatchers(HttpMethod.OPTIONS, "/**").permitAll()
+                        .pathMatchers("/profile/pets/qr-scan/**").permitAll() // <-- публичный доступ к QR
                         .anyExchange().authenticated()
                 )
                 .oauth2ResourceServer(oauth2 -> oauth2
                         .jwt(jwt -> jwt.jwtDecoder(jwtDecoder))
                 );
-
         return http.build();
     }
 }
